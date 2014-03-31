1ac06a2761bbfc34826c4e6f38ef76cfecac1359
Roy Ng <roytornado@gmail.com>
2014-03-31 10:24:19 +0800

    Revise

--- Lib/Util/ImgUtil.h
+++ Lib/Util/ImgUtil.h
@@ -3,5 +3,6 @@
 @interface ImgUtil : NSObject
 
 + (UIImage *)reize:(UIImage *)image scaledTosize:(CGSize)newSize;
++ (UIImage *)squareImage:(UIImage *)image scaledToSize:(CGFloat)size;
 
 @end
--- Lib/Util/ImgUtil.m
+++ Lib/Util/ImgUtil.m
@@ -2,12 +2,55 @@
 
 @implementation ImgUtil
 
-+ (UIImage *) reize:(UIImage*) image scaledTosize:(CGSize) newSize{
-    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
-    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
++ (UIImage *)reize:(UIImage *)image scaledTosize:(CGSize)newSize {
+	UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+	[image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+	UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	return newImage;
+}
+
++ (UIImage *)squareImage:(UIImage *)image scaledToSize:(CGFloat)size {
+	double ratio;
+	double delta;
+	CGPoint offset;
+
+	//make a new square size, that is the resized imaged width
+	CGSize sz = CGSizeMake(size, size);
+
+	//figure out if the picture is landscape or portrait, then
+	//calculate scale factor and offset
+	if (image.size.width > image.size.height) {
+		ratio = size / image.size.width;
+		delta = (ratio * image.size.width - ratio * image.size.height);
+		offset = CGPointMake(delta / 2, 0);
+	}
+	else {
+		ratio = size / image.size.height;
+		delta = (ratio * image.size.height - ratio * image.size.width);
+		offset = CGPointMake(0, delta / 2);
+	}
+
+	//make the final clipping rect based on the calculated values
+	CGRect clipRect = CGRectMake(-offset.x, -offset.y,
+	                             (ratio * image.size.width) + delta,
+	                             (ratio * image.size.height) + delta);
+
+
+	//start a new context, with scale factor 0.0 so retina displays get
+	//high quality image
+	if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
+		UIGraphicsBeginImageContextWithOptions(sz, YES, 0.0);
+	}
+	else {
+		UIGraphicsBeginImageContext(sz);
+	}
+	UIRectClip(clipRect);
+	[image drawInRect:clipRect];
+	UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+
+	return newImage;
 }
 
 @end
