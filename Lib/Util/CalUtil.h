099e53a0b3bd77de92d5aba69f350dd7f9d21f83
Roy Ng <roytornado@gmail.com>
2014-07-15 17:43:00 +0800

    Revise

--- Lib/Util/CalUtil.h
+++ Lib/Util/CalUtil.h
@@ -2,10 +2,8 @@
 
 @interface CalUtil : NSObject
 
-+ (CGFloat)calHeight:(NSString*)src font:(UIFont*)font width:(CGFloat)width min:(CGFloat)min;
++ (CGFloat)calHeight:(NSString *)src font:(UIFont *)font width:(CGFloat)width min:(CGFloat)min;
 
-+ (CGFloat)calWidth:(NSString*)src font:(UIFont*)font width:(CGFloat)width min:(CGFloat)min;
-
-+ (CGSize)calAttrSize:(NSString*)src constrain:(CGSize)constrain;
++ (CGFloat)calWidth:(NSString *)src font:(UIFont *)font width:(CGFloat)width min:(CGFloat)min;
 
 @end
