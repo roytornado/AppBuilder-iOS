6d54d93217dd7075d738fabd72317b03c1f2d503
Roy Ng <roytornado@gmail.com>
2014-09-10 16:02:57 +0800

    Revise

--- ShowCase/InfoDemoViewController.m
+++ ShowCase/InfoDemoViewController.m
@@ -8,60 +8,59 @@
 
 #import "InfoDemoViewController.h"
 #import "InfoGroup.h"
-#import "KGModal.h"
 
 @implementation InfoDemoViewController {
-	InfoGroup* infoGroup;
-	InfoSection* infoSection1;
-	InfoSection* infoSection2;
+    InfoGroup *infoGroup;
+    InfoSection *infoSection1;
+    InfoSection *infoSection2;
 
-	InfoInputText* infoInput1;
-	InfoTextH* infoText1;
-	InfoTextV* infoText2;
-	InfoInputNext* infoNext1;
+    InfoInputText *infoInput1;
+    InfoTextH *infoText1;
+    InfoTextV *infoText2;
+    InfoInputNext *infoNext1;
 }
 
 @synthesize table;
 
-- (void)viewDidLoad {
-	[super viewDidLoad];
-	infoGroup = [InfoGroup createWithTable:table];
-	infoGroup.delegate = self;
-	infoSection1 = [InfoSection createToGroup:infoGroup header:@"Info Header 1" footer:@"Info Footer 1"];
-	infoSection2 = [InfoSection createToGroup:infoGroup header:@"Info Header 2" footer:@"Info Footer 2"];
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    infoGroup = [InfoGroup createWithTable:table];
+    infoGroup.delegate = self;
+    infoSection1 = [InfoSection createToGroup:infoGroup header:@"Info Header 1" footer:@"Info Footer 1"];
+    infoSection2 = [InfoSection createToGroup:infoGroup header:@"Info Header 2" footer:@"Info Footer 2"];
 
 
-	infoInput1 = [InfoInputText createToSection:infoSection1 WithKey:@"Input 1" WithValue:@"" WithType:InfoInputTextTypeName];
-	infoText1 = [InfoTextH createToSection:infoSection1 WithKey:@"Text 1" WithValue:@"Value 1"];
-	infoText2 = [InfoTextV createToSection:infoSection1 WithKey:@"Text 2" WithValue:@"Value 2"];
-	infoNext1 = [InfoInputNext createToSection:infoSection1 WithKey:@"Next 1" WithValue:@"Value" WithTag:@""];
-	[InfoTextH createToSection:infoSection1 WithKey:@"Static Text" WithValue:@"Static Value"];
-	[InfoTextH createToSection:infoSection1 WithKey:@"Static Text" WithValue:@"Static Value"];
+    infoInput1 = [InfoInputText createToSection:infoSection1 WithKey:@"Input 1" WithValue:@"" WithType:InfoInputTextTypeName];
+    infoText1 = [InfoTextH createToSection:infoSection1 WithKey:@"Text 1" WithValue:@"Value 1"];
+    infoText2 = [InfoTextV createToSection:infoSection1 WithKey:@"Text 2" WithValue:@"Value 2"];
+    infoNext1 = [InfoInputNext createToSection:infoSection1 WithKey:@"Next 1" WithValue:@"Value" WithTag:@""];
+    [InfoTextH createToSection:infoSection1 WithKey:@"Static Text" WithValue:@"Static Value"];
+    [InfoTextH createToSection:infoSection1 WithKey:@"Static Text" WithValue:@"Static Value"];
 
-	[InfoTextH createToSection:infoSection2 WithKey:@"Static Text" WithValue:@"Static Value"];
-	[InfoTextV createToSection:infoSection2 WithKey:@"Static Text" WithValue:@"Static Value"];
-	[InfoTextH createToSection:infoSection2 WithKey:@"Static Text" WithValue:@"Static Value"];
+    [InfoTextH createToSection:infoSection2 WithKey:@"Static Text" WithValue:@"Static Value"];
+    [InfoTextV createToSection:infoSection2 WithKey:@"Static Text" WithValue:@"Static Value"];
+    [InfoTextH createToSection:infoSection2 WithKey:@"Static Text" WithValue:@"Static Value"];
 
-	[table reloadData];
+    [table reloadData];
 
-	UITextView* popView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 180, 80)];
-	popView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
-	popView.textAlignment = NSTextAlignmentCenter;
-	popView.textColor = [UIColor whiteColor];
-	popView.text = @"Hello";
-
-	[[KGModal sharedInstance] showWithContentView:popView andAnimated:YES];
+    UITextView *popView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 180, 80)];
+    popView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    popView.textAlignment = NSTextAlignmentCenter;
+    popView.textColor = [UIColor whiteColor];
+    popView.text = @"Hello";
 }
 
-- (UIView*)sectionHeaderView:(InfoSection*)section {
-	UIView* headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, table.bounds.size.width, 20)];
-	UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake(16, 0, headerView.frame.size.width - 16, 20)];
-	label.text = section.header;
-	label.font = [UIFont boldSystemFontOfSize:14];
-	label.textColor = [UIColor blueColor];
-	label.backgroundColor = [UIColor clearColor];
-	[headerView addSubview:label];
-	return headerView;
+- (UIView *)sectionHeaderView:(InfoSection *)section
+{
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, table.bounds.size.width, 20)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(16, 0, headerView.frame.size.width - 16, 20)];
+    label.text = section.header;
+    label.font = [UIFont boldSystemFontOfSize:14];
+    label.textColor = [UIColor blueColor];
+    label.backgroundColor = [UIColor clearColor];
+    [headerView addSubview:label];
+    return headerView;
 }
 
 @end
